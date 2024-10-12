9428a50b5f5f17129def384a861e4b9b05804790
romilarita85 <romico_85@outlook.com.ar>
2024-10-12 20:39:34 -0300

    primer commit

---
+++ RominaCompara/Clase08_10_Intro/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Clase08_10_Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables Escalares:
+            ////Agarrar un programa q pida 5 numeros y sumarlo
+            //int numero;
+            //int suma = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Ingrese un numero: ");
+            //    numero = int.Parse(Console.ReadLine());
+            //    suma = numero + suma;
+            //}
+            //Console.WriteLine($"La suma es: {suma}");
+            //-Estos numeros no se pueden ordenar 
+            //-No se pueden mostrar despues de ingresarlos
+            //Por que la variable (numero) con la que trabajamos es una variable escalar
+            //Variable escalar -> una variable que me permite guardar solo un valor y
+            //al iterar el for guardamos otro valor y pisamos el anterior
+            //En memoria no queda un registro de lo q estamos guardando.
+            #endregion
+
+            //APLICANCO VECTORES:
+            #region Declaracion del vector y inicializacion:
+
+            //int[] numeros = { 5, 9, 7 };//harcodeo el vector
+
+            //Console.WriteLine(numeros);//numeros -> es una direccion de memoria y C# no permite
+            ////mostrar direcciones de memoria
+            ////{} -> indica un conjunto
+
+            //Console.WriteLine(numeros[0]);//variable independiente
+            //Console.WriteLine(numeros[1]);//
+            //Console.WriteLine(numeros[2]);//
+            //                              //Console.WriteLine(numeros[3]); //->FUERA DE LOS LIMITES DEL VECTOR
+
+            ////Poder manipular cada elemento como si fuera una variable independiente
+            #endregion
+            #region Escalar en un for
+            //Escalar en un for:*****************************************
+            //Console.WriteLine("Escalar en un for:*************************");
+            //int[] numeros = { 5, 9 ,7 };
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine(numeros[i]);
+            //}
+            #endregion
+            //**************************************************************
+            //Puedo tener un for para mostrar,un for para cargar, un for para ordenar etc.
+
+            //int [] numeros = {5,2,7,4,9,8 }; // vector harcodeado: le di valores iniciales
+            
+            int[] numeros = new int[10];//crear vector vacio
+
+            CargarVector(numeros);
+
+            //int suma = 0;
+            //int sumaPares = 0;
+
+            //Console.WriteLine(numeros.Length);
+            #region Carga Secuencial de un vector:
+            //for (int i = 0; i < numeros.Length; i++)
+            //{
+            //    Console.WriteLine("Ingrese un numero: ");
+            //    numeros[i] = int.Parse(Console.ReadLine());
+            //}
+            ////Los vectores se pueden cargar de dos formas: secuencial y Aleatoria
+            ////Carga Secuencial: yo voy a recorrer cada elementos y a cada elemento le doy un valor
+            ////paso desde el primero hasta el ultimo elemento.
+            #endregion
+
+            //For para mostrar vector:(muestra los numeros)****************
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);//muesta la lista de numeros
+            }
+            #region For para la suma*************************
+            //for (int i = 0; i < numeros.Length; i++)
+            //{
+            //    suma += numeros[i];
+            //}
+            //Console.WriteLine($"La suma es: {suma}");
+            #endregion
+            #region For para sumar todos los pares:*****************************
+            //for (int i = 0; i < numeros.Length; i++)
+            //{
+            //    if (numeros[i] % 2 == 0) 
+            //    {
+            //        sumaPares += numeros[i];
+            //    }
+            //}
+            //Console.WriteLine($"La suma de los numeros pares es: {sumaPares}");
+            #endregion
+            #region For para mostrar vector al reves
+            ////Mostrar vector al reves:(de atras para adelante)**************
+            //for (int i = numeros.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(numeros[i]);
+            //}
+            #endregion
+        
+        }
+        //Funcion para cargar un vector con numeros enteros (Carga secuencial de un vector)
+        static void CargarVector(int[] numeros) 
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        //Funcion para mostrar
+        static void MostrarVector(int[] numeros) 
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);//muesta la lista de numeros
+            }
+        }
+        //Funcion para sumar
+        static void Sumar(int[] numeros) 
+        {
+            int suma = 0;
+            for (int i = 0; i < numeros.Length; i++) 
+            {
+                suma += numeros[i];
+            }
+        }
+        //Funcion para sumar pares
+        static void SumarPares(int[] numeros) 
+        {
+            int sumaPares = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] % 2 == 0) 
+                {
+                    sumaPares += numeros[i];
+                }
+            }
+        }
+        //Funcion para mostrar vector reversa
+        static void MostrarVectorReversa(int[] numeros) 
+        {
+            for (int i = numeros.Length - 1; i >= 0 ; i--)
+            {
+                Console.WriteLine(numeros[i]);//muesta la lista de numeros
+            }
+        }
+    }
+}
---
+++ RominaCompara/Clase10_10/Program.cs
@@ -0,0 +1,353 @@
+﻿namespace Clase10_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //VECTORES NUMERICOS:
+            //Si queremos agrupar toda la cantidad de datos dentro de una misma variable esa variable es un vector.
+            #region FORMAS DE CREAR UN ARRAY:
+            ////1)-Forma de crear Array vacio de 5 elementos:
+            //int[] numeros = new int[5];
+
+            ////2)-Forma de crar Array definiendo que valores va a tener adentro:
+            //int[] numeros2 = {12,15,78,5,25 };
+
+            ////Los arrays son inmutables: si yo le defini 5 unidades y siempre va a tener 5 unidades.
+            ////puede pasar q esas unidades esten vacias.
+            #endregion
+
+            #region FORMAS DE CARGAR UN ARRAY
+
+            //int[] numeros = new int[5];//array con 5 posiciones vacias
+
+            /////Numeros cargados de forma aleatoria:
+            ////numeros[0] = 54;
+            ////numeros[1] = 82;
+            ////numeros[2] = 10;
+            ////numeros[3] = 52;
+            ////numeros[4] = 12;
+
+            /////Numeros cargados en forma parcial y de manera aleatoria
+
+            //numeros[3] = 52;
+            //numeros[0] = 54;
+
+            /////No estoy obligado a darle valor a todas las posiciones
+            /////podemos cargar todas las posiciones de entrada, podemos cargar algunas o salteado
+            /////Las asignaciones son destructivas: siempre se queda con el ultimo valor
+            //numeros[3] = 18;
+            //numeros[0] = 5;
+            #endregion
+
+            #region CARGAR UN ARRAY-RECORRER CON FOR-EJEMPLO 1
+            //1-EJEMPLO 1:
+            //int[] numeros = new int[5];
+
+            //numeros[0] = 54;
+            //numeros[3] = 52;
+            //numeros[2] = 10;
+            //numeros[4] = 12;
+            //numeros[1] = 82;
+            ////el orden q lo cargue no importa siempre tiene en cuenta las posiciones
+
+            ////for Para recorrer el array
+            //for (int i = 0; i < numeros.Length; i++)
+            //{
+            //    Console.WriteLine(numeros[i]);
+            //}
+            ////Lenght: devuelve la cantidad maxima de posiciones que tiene mi array
+            #endregion
+            #region CARGAR UN ARRAY-RECORRER CON FOR-EJEMPLO 2
+            //EJEMPLO 2:
+            //int[] vectoresDeNumeros = new int[5];
+
+            //vectoresDeNumeros[4] = 12;
+            //vectoresDeNumeros[2] = 10;
+
+            ////el orden q lo cargue no importa siempre tiene en cuenta las posiciones
+            ////for Para recorrer el array
+            //for (int i = 0; i < vectoresDeNumeros.Length; i++)
+            //{
+            //    Console.WriteLine(vectoresDeNumeros[i]);
+            //}
+            #endregion
+            //*****************************************************************
+            #region CARGAR UN ARRAY-RECORRER CON FOR-EJEMPLO 3
+            ////EJEMPLO 3: OTRA FORMA:
+            // int tamanio = 5;
+            // int[] vectoresDeNumeros = new int[tamanio];
+
+            // vectoresDeNumeros[4] = 12;
+            // vectoresDeNumeros[2] = 10;
+
+            // for (int i = 0; i < tamanio; i++)
+            // {
+            //     Console.WriteLine(vectoresDeNumeros[i]);
+            // }
+            #endregion
+            //****************************************************************
+            #region HACER UN ARRAY QUE PIDAR INGRESAR NUMEROS Y GUARDARLO EN FORMA CONSECUTIVA-EJEMPLO 4
+            ////EJEMPLO 4: 
+
+            //int[] vectorDeNumeros = new int[5];
+
+            //vectorDeNumeros[4] = 12;
+            //vectorDeNumeros[2] = 10;
+
+            ////CARGA SECUENCIAL: Hubo una secuencia ordenada y le pido un dato al usuario
+            //for (int i = 0; i < vectorDeNumeros.Length; i++)
+            //{
+            //    Console.WriteLine($"Ingrese el numero {i + 1} de {vectorDeNumeros.Length}");//linea para decirle al usuario q ingrese el numero
+
+            //    int numeroIngresado = int.Parse(Console.ReadLine());//lee (parseo)
+            //    vectorDeNumeros[i] = numeroIngresado; //(Asigna) Para guardar el numero ingresado en cada posicion de i
+
+            //    //string lectura = Console.ReadLine();
+            //    //int numeroIngresado = int.Parse(lectura); //lee
+            //    //vectorDeNumeros[i] = numeroIngresado; //asgina
+            //}
+            ////CARGA ALEATORIA: PODEMOS HACERLO DE MANERA ALEATORIA:
+            ////La asinacion es destructiva -> si el usuario elige una posicion q tenia un valor ese numero se va a pisar
+            ////-Tenemos q pedir el numero de posicion en q lo queremos guardar
+            ////-el numero q quiere guardar en esa posicion
+            //***************************
+            #endregion
+
+            #region USANDO FUNCIONES CON ARRAY Ejemplo 5
+            //int[] vectorDeNumeros = new int[5];
+            //CargarVector(vectorDeNumeros);
+            //Console.WriteLine("*************MOSTRAR*************");
+            //MostrarVector(vectorDeNumeros);
+
+            //Console.WriteLine("*************MOSTRAR SOLO PARES*************");
+            //MostrarPares(vectorDeNumeros);
+
+            //Console.WriteLine("*************SUMANDO POSITIVOS*************");
+            //Console.WriteLine($"El valor de la suma de positivos: {SumarPositivos(vectorDeNumeros)}");
+            #endregion
+
+            #region USANDO FUNCION ORDENAR VECTOR-VUELTAS DE i y j.Ejemplo 6 
+            //int[] vectorDeNumeros = { 25, 12, 85, -12, 6 }; // doy valor de entrada
+
+            //int auxiliar;
+            //MostrarVector(vectorDeNumeros);
+
+            //for (int i = 0; i < vectorDeNumeros.Length; i++)
+            //{
+            //    Console.WriteLine($"Vuelta en {i}");
+
+            //    for (int j = 0; j < vectorDeNumeros.Length; j++)
+            //    {
+            //        Console.WriteLine($"Vuelta en {j}");
+
+            //        //Comparacion de posiciones
+            //        if (vectorDeNumeros[i] < vectorDeNumeros[j]) //poder mover los numeros
+            //        {
+            //            //Comparar q posicion es mayor q la q sigue
+            //            Console.WriteLine(vectorDeNumeros[i]);
+            //            Console.WriteLine(vectorDeNumeros[j]);
+            //        }
+            //    }
+            //}
+            #endregion
+
+            #region USANDO FUNCION ORDENAR VECTOR-CONTADOR-EJEMPLO 7
+            ////Ejemplo 6:
+            //int[] vectorDeNumeros = { 25,12,85,-12,6}; // doy valor de entrada
+
+            //int auxiliar ;
+            //int contador = 0;
+            //Console.WriteLine("Mostrar antes de ordenar*******");
+            //MostrarVector(vectorDeNumeros);
+
+            //for (int i = 0; i < vectorDeNumeros.Length; i++)
+            //{
+            //    for (int j = 0; j < vectorDeNumeros.Length; j++)
+            //    {
+            //        //Comparacion de posiciones
+            //        if (vectorDeNumeros[i] < vectorDeNumeros[j]) //-> si quiero ordenar al reves: (vectorDeNumeros[i] > vectorDeNumeros[j])
+            //        {//poder mover los numeros
+            //            //Comparar q posicion es mayor q la q sigue
+
+            //            //necesito una variable interna auxiliar
+            //            auxiliar = vectorDeNumeros[i];
+            //             //guardo lo de i en una variable auxiliar
+            //            vectorDeNumeros[i] = vectorDeNumeros[j];
+            //            // lo piso
+            //            vectorDeNumeros[j] = auxiliar;
+            //            //el valor q tenia en j lo recupero con el auxiliar
+
+            //        }
+            //        contador++;
+            //    }
+            //}
+            //Console.WriteLine("Mostrar despues de ordenar*******");
+            //MostrarVector(vectorDeNumeros);
+            //Console.WriteLine($"Cantidad de vueltas {contador}");
+            ////por cada vuelta que dio recorrio la 5 posiciones del vector
+            #endregion
+            #region USANDO FUNCION ORDENAR VECTOR EJEMPLO 8(CAMBIANDO VALOR DE J -> j= i+1 EN FOR DE J)
+            ////Ejemplo 8:)
+            //int[] vectorDeNumeros = { 25, 12, 85, -12, 6 }; // doy valor de entrada
+
+            //int auxiliar;
+            //Console.WriteLine("Mostrar antes de ordenar*******");
+            //MostrarVector(vectorDeNumeros);
+
+            //for (int i = 0; i < vectorDeNumeros.Length; i++)
+            //{
+            //    for (int j = i + 1; j < vectorDeNumeros.Length; j++)
+            //    {
+            //        //Comparacion de posiciones
+            //        if (vectorDeNumeros[i] < vectorDeNumeros[j]) //-> si quiero ordenar al reves: (vectorDeNumeros[i] > vectorDeNumeros[j])
+            //        {//poder mover los numeros
+            //         //Comparar q posicion es mayor q la q sigue
+
+            //            //necesito una variable interna auxiliar
+            //            auxiliar = vectorDeNumeros[i];
+            //            //guardo lo de i en una variable auxiliar
+            //            vectorDeNumeros[i] = vectorDeNumeros[j];
+            //            // lo piso
+            //            vectorDeNumeros[j] = auxiliar;
+            //            //el valor q tenia en j lo recupero con el auxiliar
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("Mostrar despues de ordenar*******");
+            //MostrarVector(vectorDeNumeros);
+            #endregion
+            
+            //USANDO FUNCION ORDENAR VECTOR-MAYOR A MENOR/MENOR A MAYOR
+            //EJEMPLO 9:
+            int[] vectorDeNumeros = { 25, 12, 85, -12, 6 }; // doy valor de entrada
+
+            int auxiliar;
+            Console.WriteLine("Mostrar antes de ordenar*******");
+            MostrarVector(vectorDeNumeros);
+
+            OrdenarVector1(vectorDeNumeros,true);//MUESTRA DE MENOR A MAYOR
+            OrdenarVector1(vectorDeNumeros, false);//MUESTRA DE MAYOR A MENOR
+
+            Console.WriteLine("Mostrar despues de ordenar*******");
+            MostrarVector(vectorDeNumeros);
+        }
+
+        //FUNCION PARA CARGAR VECTORES:
+        public static void CargarVector(int[] vector) 
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine($"Ingrese el numero {i + 1} de {vector.Length}");//linea para decirle al usuario q ingrese el numero
+
+                int numeroIngresado = int.Parse(Console.ReadLine());//lee (parseo)
+                vector[i] = numeroIngresado; //(Asigna) Para guardar el numero ingresado en cada posicion de i
+
+            }
+        }
+
+        //FUNCION PARA MOSTRAR VECTOR:
+        public static void MostrarVector(int[] vector) 
+        {
+            foreach (int elementos in vector)
+            {
+                Console.WriteLine(elementos);//en cada vuelta imprime el elemento
+            }
+
+            //for (int i = 0; i < vector.Length; i++)
+            //{
+            //    Console.WriteLine(vector[i]);
+            //}
+        }
+
+        //FUNCION PARA MOSTRAR NUMEROS PARES:
+        public static void MostrarPares(int[] vector) 
+        {
+            foreach (int elemento in vector)
+            {
+                if (elemento % 2 == 0)
+                {
+                    Console.WriteLine(elemento);
+                }
+            }
+        }
+
+        //FUNCION PARA SUMAR POSITIVOS:
+        public static int SumarPositivos(int[] vector) 
+        {
+            int suma = 0;
+            foreach (int elemento in vector)
+            {
+                if (elemento > 0)
+                {
+                    suma += elemento;
+                }
+            }
+            return suma;
+        }
+        //FUNCION PARA ORDENAR VECTOR
+        public static void OrdenarVector(int[] vector) 
+        {
+            int auxiliar;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                for (int j = i + 1; j < vector.Length; j++)
+                {
+                    //Comparacion de posiciones
+                    if (vector[i] < vector[j]) //-> si quiero ordenar al reves: (vectorDeNumeros[i] > vectorDeNumeros[j])
+                    {
+                        auxiliar = vector[i];
+                        vector[i] = vector[j];
+                        vector[j] = auxiliar;
+                        
+                    }
+                }
+            }
+        }
+        //FUNCION PARA ORDENAR DE MENOR-MAYOR/MAYOR-MENOR
+        public static void OrdenarVector1(int[] vector, bool ascendente)
+        {
+            int auxiliar;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                for (int j = i + 1; j < vector.Length; j++)
+                {
+                    if (ascendente)//si ascendente es verdadero
+                    {
+                        if (vector[i] > vector[j]) //SI ASCENDENTE ESTA EN TRUE : ME LO MUESTA DE MENOR A MAYOR
+                        {
+                            auxiliar = vector[i];
+                            vector[i] = vector[j];
+                            vector[j] = auxiliar;
+                        }
+                    }
+                    else 
+                    {
+                        if (vector[i] < vector[j]) //SI ASCENDENTE ESTA EN FALSE : ME LO MUESTA DE MAYOR A MENOR
+                        {
+                            auxiliar = vector[i];
+                            vector[i] = vector[j];
+                            vector[j] = auxiliar;
+                        }
+                    }  
+                }
+            }
+        }
+    }
+}
+//namespace Clase10_10
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //VECTORES NUMERICOS:
+//            //Para guardar numeros: creabamos variables de ese tipo y guardabamos un dato.
+//            int unNumero = 8;//variable de tipo entero a la cual se le asigna un unico valor 
+//            int otroNumero = 10;
+
+//            //Si queremos agrupar toda la cantidad de datos dentro de una misma variable esa variable es un vector.
+          
+//        }
+//    }
+//}
